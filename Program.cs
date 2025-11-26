ff7c0c2372f3383819426842a8aec5bdb7c6bd2c
Raul <rap.gardini@gmail.com>
2025-11-26 16:00:23 -0300

    .

--- Program.cs
+++ Program.cs
@@ -9,14 +9,27 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// ADICIONE ESTA LINHA - Configuração do CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "http://localhost:5174", "http://localhost:3001")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // registra o agente como Singleton
 builder.Services.AddSingleton<OpenAIAgentService>();
 
 var app = builder.Build();
 
+// ADICIONE ESTA LINHA - Usar o CORS (ANTES de MapControllers)
+app.UseCors("AllowReactApp");
+
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.MapControllers();
 
 app.Run();
